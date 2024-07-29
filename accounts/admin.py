3c007755cd6bd40eef2a8e8e9d751cdc7964d2fb
Adam <adam.nemmish213@gmail.com>
2024-07-30 00:58:47 +0600

    Выполнил 1 этап: Описал свою модель юзера Добавил поля: ' Логин Адрес электронной почты Аватар Пароль Имя Информация о пользователе Номер телефона Пол' Так же кол-во публикаций, подписок, и подписчиков

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from accounts.models import MyUser
 
-# Register your models here.
+
+@admin.register(MyUser)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'username']
+    list_filter = ['id', 'username']
+    search_fields = ['username', 'id']
+    fields = ['username', 'email', 'avatar', 'password', 'first_name', 'info', 'phone', 'gender']
+
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+GENDER_CHOICES = [
+    ('Male', 'Мужчина'),
+    ('Female', 'Женщина'),
+    ('Other', 'Другое')
+]
+
+
+class MyUser(AbstractUser):
+    email = models.EmailField(unique=True, verbose_name="Email", max_length=254)
+    avatar = models.ImageField(upload_to='avatars/', verbose_name='Avatar')
+    info = models.TextField(verbose_name='Info', max_length=500, blank=True, null=True)
+    phone = models.CharField(max_length=20, verbose_name="Phone Number", blank=True, null=True)
+    gender = models.CharField(choices=GENDER_CHOICES, verbose_name="Gender", blank=True, null=True, max_length=20)
+    publication_count = models.PositiveIntegerField(default=0, verbose_name='Publications')
+    subscription_count = models.PositiveIntegerField(default=0, verbose_name='Subscriptions')
+    following_count = models.PositiveIntegerField(default=0, verbose_name='Following')
+
+    def __str__(self):
+        return f'{self.username}{self.gender}'
