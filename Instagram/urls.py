b272bf308bc4ac0800c50fdbd6b6137f8b79dd7e
Adam <adam.nemmish213@gmail.com>
2024-08-26 21:51:55 +0600

    Добавил новое приложени api_v1 в проект Добавил файл джс для лайков

--- Instagram/urls.py
+++ Instagram/urls.py
@@ -19,8 +19,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+api_routers = [
+    path('v1/', include('api_v1.urls')),
+]
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('webapp.urls')),
     path('accounts/', include('accounts.urls')),
+    path('api/', include(api_routers)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ api_v1/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+app_name = 'api_v1'
+
+urlpatterns = [
+
+]
