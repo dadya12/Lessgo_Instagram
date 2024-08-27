ca9534d9fbfeb25045508c793b725073e5648066
Adam <adam.nemmish213@gmail.com>
2024-08-27 22:03:45 +0600

    Реализовал разрешения Так же написал кастомное разрешение для удаления и редактирование только для авторов

--- api_v1/views.py
+++ api_v1/views.py
@@ -1,14 +1,16 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets
 from rest_framework.response import Response
-
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from webapp.models import Post
 from .serializer import PostSerializer
 from rest_framework.decorators import action
+from api_v1.castom_perms.perms import IsOwnerOrReadOnly
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
