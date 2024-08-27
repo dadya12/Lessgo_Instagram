9b15d8f160f72e9fe87a697fa2c09753b17dbfe9
Adam <adam.nemmish213@gmail.com>
2024-08-27 13:26:18 +0600

    Реализовал удаление и добавления лайков

--- api_v1/views.py
+++ api_v1/views.py
@@ -1,6 +1,9 @@
 from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+
 from webapp.models import Post
 from .serializer import PostSerializer
+from rest_framework.decorators import action
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -9,3 +12,17 @@ class PostViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+    @action(methods=['POST'], detail=True)
+    def like(self, request, pk=None):
+        post = self.get_object()
+        post.users_likes.add(request.user)
+        post.save()
+        return Response({"post_liked": pk})
+
+    @action(methods=['POST'], detail=True)
+    def unlike(self, request, pk=None):
+        post = self.get_object()
+        post.users_likes.remove(request.user)
+        post.save()
+        return Response({"post_unliked": pk})
